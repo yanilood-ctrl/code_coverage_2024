b94c098282a4bd63fa38dd99c5dd3614cc84a20e
yanilood-ctrl <yanilood@gmail.com>
2025-12-23 00:52:32 +0200

    ggg

--- code_coverage_4/src/test/java/code_coverage_4/code_coverage_4/ActivityDecisionTest.java
+++ code_coverage_4/src/test/java/code_coverage_4/code_coverage_4/ActivityDecisionTest.java
@@ -5,28 +5,29 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class ActivityDecisionTest {
+	ActivityDecision x=new ActivityDecision();
 
     @Test
     public void walkingMidnightRaining() {
-        String result = ActivityDecision.decideActivity(true, true, false, false, true);
+        String result = x.decideActivity(true, true, false, false, true);
         assertEquals("Take umbrella and searchlight", result);
     }
 
     @Test
     public void walkingMidnightNotRaining() {
-        String result = ActivityDecision.decideActivity(true, true, false, false, false);
+        String result = x.decideActivity(true, true, false, false, false);
         assertEquals("Take searchlight", result);
     }
 
     @Test
     public void runningSunshineRaining() {
-        String result = ActivityDecision.decideActivity(false, false, true, true, true);
+        String result = x.decideActivity(false, false, true, true, true);
         assertEquals("Take umbrella", result);
     }
 
     @Test
     public void defaultCase() {
-        String result = ActivityDecision.decideActivity(false, false, false, false, false);
+        String result = x.decideActivity(false, false, false, false, false);
         assertEquals("Keep doing what you were doing", result);
     }
 }
