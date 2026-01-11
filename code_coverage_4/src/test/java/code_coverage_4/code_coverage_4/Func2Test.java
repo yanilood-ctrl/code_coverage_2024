1ca097d7a6c7dd5f1ec96541e771da60db488fcd
yanilood-ctrl <yanilood@gmail.com>
2026-01-11 17:34:22 +0200

    ب

--- code_coverage_4/src/test/java/code_coverage_4/code_coverage_4/Func2Test.java
+++ code_coverage_4/src/test/java/code_coverage_4/code_coverage_4/Func2Test.java
@@ -5,17 +5,18 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 public class Func2Test {
+	Func2 f2=new Func2();
 
     @Test
     public void T1_p5_q30() {
-        String result = Func2.func2(5f, 30f);
+        String result = f2.func2(5f, 30f);
         // p = 15.00, q = 2.00
         assertEquals("p=15.00, q=2.00", result);
     }
 
     @Test
     public void T2_p0_q30_infinity() {
-        String result = Func2.func2(0f, 30f);
+        String result = f2.func2(0f, 30f);
         // في float القسمة على 0 تعطي Infinity (ليس Exception)
         assertTrue("Expected Infinity in result, got: " + result, result.contains("Infinity"));
     }
--- code_coverage_4/src/test/java/code_coverage_4/code_coverage_4/Func3Test.java
+++ code_coverage_4/src/test/java/code_coverage_4/code_coverage_4/Func3Test.java
@@ -3,9 +3,10 @@ package code_coverage_4.code_coverage_4;
 import org.junit.Test;
 
 public class Func3Test {
+	Func3 f3=new Func3();
 
     @Test(expected = ArithmeticException.class)
     public void T1_x1_shouldThrow() {
-        Func3.func3(1);
+    	f3.func3(1);
     }
 }
